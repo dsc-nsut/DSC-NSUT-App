1f97e7fcf76ef12b955fba732fb101dc63dc7d9e
visheshnaithani <68864105+visheshnaithani@users.noreply.github.com>
2021-04-06 18:25:36 +0530

    Update MainActivity.kt

--- app/src/main/java/com/dscnsut/app/MainActivity.kt
+++ app/src/main/java/com/dscnsut/app/MainActivity.kt
@@ -1,22 +1,11 @@
 package com.dscnsut.app
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.dscnsut.app.members.MembersViewModel
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val viewmodel = ViewModelProvider(this).get(MembersViewModel::class.java)
-        viewmodel.getMembers()
-        viewmodel.members.observe(this, {membersList->
-        if(membersList.isEmpty()) {
-            println("List is Empty")
-        }
-        else
-            println(membersList)
-        })
     }
 }
