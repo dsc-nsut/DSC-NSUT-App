8586b1c259733e597850d62db6d9d892cb3e99e5
Harshita28-01 <78307404+Harshita28-01@users.noreply.github.com>
2021-04-09 21:11:26 +0530

    Bottom Navigation Bar

---
+++ MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dscnsut.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        navController = findNavController(R.id.nav_host_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
