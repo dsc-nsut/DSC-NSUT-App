718acdeaa4b8eca33271536816413e632ad2de9c
Crimson Deadra <60355338+onerouskin@users.noreply.github.com>
2021-04-09 10:49:56 +0530

    Update BlogContent.kt

--- app/src/main/java/com/dscnsut/app/BlogContent.kt
+++ app/src/main/java/com/dscnsut/app/BlogContent.kt
@@ -20,7 +20,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [blog_content.newInstance] factory method to
  * create an instance of this fragment.
  */
-class blog_content : Fragment() {
+class BlogContent : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
