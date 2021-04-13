53bec93b3b8cc1cd15c4c097a5f49a49b6d29405
onerouskin <onerouskin@gmail.com>
2021-04-13 12:18:59 +0530

    Blog Details fragment Commit:1

--- app/src/main/java/com/dscnsut/app/blogs/BlogsFragment.kt
+++ app/src/main/java/com/dscnsut/app/blogs/BlogsFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.dscnsut.app.events.EventsFragment
 import com.dscnsut.app.R
 
@@ -36,9 +40,24 @@ class BlogsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false)
+        return inflater.inflate(R.layout.fragment_blogs, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        view.findViewById<Button>(R.id.nav_btn_blogDetails)?.setOnClickListener {
+            findNavController().navigate(R.id.fragment_blogDetails, null, options)
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
--- app/src/main/java/com/dscnsut/app/main/MainActivity.kt
+++ app/src/main/java/com/dscnsut/app/main/MainActivity.kt
@@ -2,8 +2,11 @@ package com.dscnsut.app.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import androidx.navigation.ui.setupWithNavController
 import com.dscnsut.app.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -15,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         navController = findNavController(R.id.nav_host_fragment)
-//        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
+
+
+
+
     }
 }
