da2cb73900a1fe206821c78b73edb1227f9bd110
onerouskin <onerouskin@gmail.com>
2021-04-15 18:01:24 +0530

    Blog Details fragment Commit:1

--- app/src/main/java/com/dscnsut/app/blogs/BlogsFragment.kt
+++ app/src/main/java/com/dscnsut/app/blogs/BlogsFragment.kt
@@ -34,7 +34,7 @@ class BlogsFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false)
+        return inflater.inflate(R.layout.fragment_blogs, container, false)
     }
 
     companion object {
