f2bad11752bc8072594d9ad824bcbab4240617f1
onerouskin <onerouskin@gmail.com>
2021-04-15 17:51:04 +0530

    Blog Details fragment Commit:1

--- app/src/main/java/com/dscnsut/app/blogs/BlogDetailsFragment.kt
+++ app/src/main/java/com/dscnsut/app/blogs/BlogDetailsFragment.kt
@@ -1,10 +1,14 @@
-package com.dscnsut.app
+package com.dscnsut.app.blogs
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.navigation.fragment.navArgs
+import com.dscnsut.app.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +33,22 @@ class fragment_blogDetails : Fragment() {
         }
     }
 
+    val safeArgs : fragment_blogDetailsArgs by navArgs()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blog_details, container, false)
-    }
 
+
+        val blog = safeArgs.blog
+
+        val view = inflater.inflate(R.layout.fragment_blog_details, container, false)
+        view.findViewById<TextView>(R.id.BlogHeader).setText(blog.title)
+        view.findViewById<TextView>(R.id.BlogCreator).setText(blog.author)
+        view.findViewById<TextView>(R.id.BlogDate).setText(blog.timestamp.toString())
+        view.findViewById<TextView>(R.id.BlogBody).setText(blog.body)
+
+
+        return view
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
--- app/src/main/java/com/dscnsut/app/blogs/BlogsFragment.kt
+++ app/src/main/java/com/dscnsut/app/blogs/BlogsFragment.kt
@@ -1,14 +1,8 @@
-package com.dscnsut.app.blogs
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.navOptions
 import com.dscnsut.app.events.EventsFragment
 import com.dscnsut.app.R
 
@@ -36,28 +30,13 @@ class BlogsFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blogs, container, false)
+        return inflater.inflate(R.layout.fragment_events, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val options = navOptions {
-            anim {
-                enter = R.anim.slide_in_right
-                exit = R.anim.slide_out_left
-                popEnter = R.anim.slide_in_left
-                popExit = R.anim.slide_out_right
-            }
-        }
-        view.findViewById<Button>(R.id.nav_btn_blogDetails)?.setOnClickListener {
-            findNavController().navigate(R.id.fragment_blogDetails, null, options)
-        }
-    }
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -70,13 +49,11 @@ class BlogsFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            EventsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                EventsFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
-
-
